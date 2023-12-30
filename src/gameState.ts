457987d8500ee489763f496d33cf91d0d28b5962
Erik Sundberg <erik.sundberg@unicus.com>
2023-12-30 14:21:47 +0100

    refactor: cleanup

--- src/gameState.ts
+++ src/gameState.ts
@@ -6,13 +6,11 @@ import { getDefaultBoardState } from './boardState';
 export const useGameStore = create<gameStore>((set) => ({
   boardState: getDefaultBoardState(),
   selectedPearlPosition: undefined,
-  targetPit: undefined,
   pearlsRemoved: [],
   appendRemoved: (pearl) =>
     set((state) => ({ pearlsRemoved: [...state.pearlsRemoved, pearl] })),
   setSelectedPearl: (selectedPearl) =>
     set({ selectedPearlPosition: selectedPearl }),
-  setTargetPit: (targetPit) => set({ targetPit }),
   setBoardState: (boardState) => set({ boardState }),
   resetBoardState: () => set({ boardState: getDefaultBoardState(), pearlsRemoved: [] }),
 }));
@@ -20,11 +18,9 @@ export const useGameStore = create<gameStore>((set) => ({
 export type gameStore = {
   boardState: Map<string, JSX.Element>;
   selectedPearlPosition: Position | undefined;
-  targetPit: Position | undefined;
   pearlsRemoved: JSX.Element[];
   appendRemoved: (pearl: JSX.Element) => void;
   setSelectedPearl: (selectedPearl: Position) => void;
-  setTargetPit: (targetPit: Position) => void;
   setBoardState: (boardState: Map<string, JSX.Element>) => void;
   resetBoardState: () => void;
 };
